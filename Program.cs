f6006eff938af3f5cfae331ef21db15d7868042f
ching <srching23@gmail.com>
2025-09-24 19:48:03 -0500

    Implement Patient class with properties and constructor; add Main method for patient instantiation and display

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using ClinicaSalud.models;
+
+class Program
+{
+    static void Main()
+    {
+        var patients = new Patient("John Doe", 30, "Gripe");
+
+        Console.WriteLine($"Patient Name: {patients.name}");
+        Console.WriteLine($"Patient Age: {patients.age}");
+        Console.WriteLine($"Patient Symptom: {patients.symptom}");
+    }
+}
--- model/Patient.cs
+++ model/Patient.cs
@@ -3,8 +3,19 @@ namespace ClinicaSalud.models;
 
 public class Patient
 {
-    public int id { get; set; }
+    public Guid id { get; set; }
     public string name { get; set; }
     public int age { get; set; }
     public string symptom { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID: {id}, Name: {name}, Age: {age}, Symptom: {symptom}";
+    }
+    public Patient(string name, int age, string symptom)
+    {
+        this.name = name;
+        this.age = age;
+        this.symptom = symptom;
+    }
 }
